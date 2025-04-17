863c4acbf81c3fa13bab4781b4c8d762c6953f0d
marioBytes <mario.bytes92@gmail.com>
2025-04-17 08:46:52 -0600

    Switch back to gnome

--- configuration.nix
+++ configuration.nix
@@ -46,8 +46,8 @@
   services.xserver.enable = true;
 
   # Enable the Deepin Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.deepin.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
